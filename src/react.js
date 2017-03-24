e68f86ec39086e53db76b63dfd5a8d35e6131d4d
slashhuang <627284482@qq.com>
2017-03-24 17:10:36 +0800

    update react

--- src/react.js
+++ src/react.js
@@ -31,3 +31,49 @@ class FirstComponent extends Component{
     }
  };
  render(<FirstComponent />,document.getElementById('root'))
+
+
+ const highOrderFunc1=actions=>InnerComponent=>{
+    class Inst1 extends Component{
+        componentDidMount(){
+              let _ref = this.refs.InnerComponent;
+                 //侵入事件
+                 _ref.__proto__.clickFunc = function(...args){
+                    this.props.Update();
+                    let { text } = this.state;
+                    this.setState({text:`add Text ${text}`})
+                 }
+        }
+        render(){
+            //update Props
+            this.props = {
+                ...this.props,
+                ...actions
+            };
+            return <InnerComponent ref='InnerComponent' {...this.props}/>
+        }
+    }
+    return Inst1;
+}
+@highOrderFunc1({
+    'Update':()=>{console.log('Update')}
+})
+class FirstComponent1 extends Component{
+    constructor(){
+        super()
+        this.state={ text: 'hello world' }
+    }
+    clickFunc(){
+        this.setState({text:'I am clicked'})
+    }
+    render(){
+        return <div onClick={()=>this.clickFunc()}>{this.state.text}</div>
+    }
+ };
+ render(<FirstComponent1  color='red'/>,document.getElementById('root1'))
+
+
+
+
+
+
