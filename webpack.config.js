ce5c4301ffa43ff952f4666ffa340e17479a9655
slashhuang <627284482@qq.com>
2017-03-24 13:45:22 +0800

    add decorator hook

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,12 @@ var path =require('path');
 module.exports = {
     watch:true,
     entry: {
-        index:"./src/index.js"
+        index:"./src/index.js",
+        react:"./src/react.js"
         // 如果你要async await Promise的话，取消这行注释
         // ,test:['babel-polyfill',"./src/index.js"]
     },
+    devtool:'source-map',
     output: {
         publicPath:'/dist/',
         path: path.join(__dirname,'dist'),
@@ -19,7 +21,7 @@ module.exports = {
         loaders: [
             {
                 test: /\.js[x]?$/,
-                loader: "babel-loader",
+                loader: "babel",
                 exclude: /node_modules/
             }
         ],
